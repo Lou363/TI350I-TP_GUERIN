1cf9810d1bf3b624a01cd59f8587e067c0f7955d
Lou363 <77783737+Lou363@users.noreply.github.com>
2021-12-03 16:31:15 +0100

    Update sonarQube02.java

--- sonarQube02.java
+++ sonarQube02.java
@@ -1,8 +1,7 @@
 package main;
-public class SonarQube02 {
 
+public class SonarQube02 {
     String printNumbers(String stringToAdd, int x, int y) {
         return String.format("Numbers are %s %d %d", stringToAdd, x, y);
     }
-
 }
