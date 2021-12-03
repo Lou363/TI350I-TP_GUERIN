b108c5e8754e747ffaf5a6baf513cfcb2a4a3b0b
Lou363 <77783737+Lou363@users.noreply.github.com>
2021-12-03 16:06:35 +0100

    Attempted to fix merge error

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,6 +1,5 @@
-package main;
 public class SonarQube01 {
     String addHeader(String stringToAdd, int x, int y){
-        return String.format("Added Header %d %d %d", stringToAdd, x, y);
+        return String.format("Added Header %s %d %d", stringToAdd, x, y);
     }
 }
